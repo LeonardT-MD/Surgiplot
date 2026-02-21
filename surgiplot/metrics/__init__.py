10b06cc1162ad4c1abf4e33ebbac58ab637e6f2d
LeonardT-MD <77591128+LeonardT-MD@users.noreply.github.com>
2026-02-21 14:02:24 +0100

    Update __init__.py

--- surgiplot/metrics/__init__.py
+++ surgiplot/metrics/__init__.py
@@ -1,3 +1,5 @@
 from .vom_voa import VOM_VOA, VOMVOAResult
 from .aoa_sf import AOA_SF, AOASFResult
 from .aoe import AOE, AOEResult
+from .distance_3d import DISTANCE_3D, Distance3DResult
+from .area_3d import AREA_3D, Area3DResult
